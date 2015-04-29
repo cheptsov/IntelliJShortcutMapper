2cdd059f80dbfe17a420090661d2d5989bb83482
cheptsov <andrey.cheptsov@jetbrains.com>
2015-04-29 18:53:54 +0300

    Bugfixing

--- src/ShortcutExporterAction.java
+++ src/ShortcutExporterAction.java
@@ -1,4 +1,5 @@
 import com.google.common.base.Function;
+import com.google.common.base.Predicate;
 import com.google.common.collect.FluentIterable;
 import com.intellij.ide.CopyPasteManagerEx;
 import com.intellij.openapi.actionSystem.ActionManager;
@@ -26,7 +27,7 @@ public class ShortcutExporterAction extends AnAction {
         final ActionManager actionManager = ActionManagerEx.getInstance();
         final Set<String> winActionIds = new HashSet<String>(Arrays.asList(winKeymap.getActionIds()));
         final Set<String> macActionIds = new HashSet<String>(Arrays.asList(macKeymap.getActionIds()));
-        final StringBuilder stringBuilder = new StringBuilder().append("{\n").append("\t\"name\": \"JetBrains ").append(ApplicationNamesInfo.getInstance().getFullProductName()).append("\",\n").append("\t\"version\": \"").append(ApplicationInfoEx.getInstanceEx().getMajorVersion()).append("\",\n").append("\t\"default_context\": \"Global Context\",\n").append("\t\"os\": [\"windows\", \"mac\", \"linux\"],\n").append("\t\"contexts\": {\n").append("\t\t\"Global Context\": {\n");
+        final StringBuilder stringBuilder = new StringBuilder().append("{\n").append("\t\"name\": \"").append(ApplicationNamesInfo.getInstance().getFullProductName()).append("\",\n").append("\t\"version\": \"").append(ApplicationInfoEx.getInstanceEx().getMajorVersion()).append("\",\n").append("\t\"default_context\": \"Global Context\",\n").append("\t\"os\": [\"windows\", \"mac\", \"linux\"],\n").append("\t\"contexts\": {\n").append("\t\t\"Global Context\": {\n");
         final Set<String> commonActionIds = new HashSet<String>();
         commonActionIds.addAll(winActionIds);
         commonActionIds.retainAll(macActionIds);
@@ -40,23 +41,74 @@ public class ShortcutExporterAction extends AnAction {
                 public String apply(Shortcut shortcut) {
                     return shortcut.toString();
                 }
+            }).filter(new Predicate<String>() {
+                @Override
+                public boolean apply(String s) {
+                    return !s.contains("button");
+                }
             }), ",");
             final String macShortcuts = StringUtil.join(FluentIterable.from(Arrays.asList(macKeymap.getShortcuts(actionId))).transform(new Function<Shortcut, String>() {
                 @Override
                 public String apply(Shortcut shortcut) {
                     return shortcut.toString();
                 }
+            }).filter(new Predicate<String>() {
+                @Override
+                public boolean apply(String s) {
+                    return !s.contains("button");
+                }
             }), ",");
             if (action != null) {
                 final String winShortcutFormatted = formatShortcut(winShortcuts);
                 final String macShortcutFormatted = formatShortcut(macShortcuts);
-                if (!winShortcutFormatted.contains("button=") && !macShortcutFormatted.contains("button=")
-                    && !winShortcutFormatted.isEmpty() && !macShortcutFormatted.isEmpty() &&
+                final String actionName = action.getTemplatePresentation().getText().
+                        replaceAll("Class\\.\\.\\.", "Navigate to Class").
+                        replaceAll("File\\.\\.\\.", "Navigate to File").
+                        replaceAll("Symbol\\.\\.\\.", "Navigate to Symbol").
+                        replaceAll("Line\\.\\.\\.", "Navigate to Line").
+                        replaceAll("^Type Declaration$", "Navigate to Type Declaration").
+                        replaceAll("^Declaration$", "Navigate to Declaration").
+                        replaceAll("^Super Method", "Navigate to Super Method").
+                        replaceAll("^Implementation\\(s\\)$", "Navigate to Implementation(s)").
+                        replaceAll("Method\\.\\.\\.", "Extract Method").
+                        replaceAll("Constant\\.\\.\\.", "Extract Constant").
+                        replaceAll("Variable\\.\\.\\.", "Extract Variable").
+                        replaceAll("Functional Parameter\\.\\.\\.", "Extract Functional Parameter").
+                        replaceAll("Parameter\\.\\.\\.", "Extract Parameter").
+                        replaceAll("Field\\.\\.\\.", "Extract Field").
+                        replaceAll("Basic", "Basic Completion").
+                        replaceAll("Test", "Navigate to Test").
+                        replaceAll("SmartType", "Smart Completion").
+                        replaceAll("\\.\\.\\.", "").
+                        replaceAll("New Folder", "New...");
+                if (!actionName.contains("XPath") && !actionName.contains("Zoom") &&
+                        !actionName.contains("Stretch") && !actionName.contains("Run Ant Target") &&
+                        !actionName.equals("Tab") && !actionName.equals("Fully Expand Tree Node") &&
+                        !actionName.equals("Set NULL") && !actionName.equals("Add") && !actionName.equals("Send EOF") &&
+                        !actionName.equals("Up") && !actionName.equals("Down") && !actionName.equals("Configure Current File Analysis") &&
+                        !actionName.equals("Left") && !actionName.equals("Right") && !actionName.equals("Project Directory") &&
+                        !actionName.equals("Right") && !actionName.equals("Show/Hide Columns") && !actionName.equals("Maintenance") &&
+                        !actionName.equals("Generate DDL") && !actionName.equals("Open Console") && !actionName.equals("Recursive") &&
+                        !actionName.equals("Execute Groovy Code") && !actionName.equals("Go To Changed File") &&
+                        !actionName.equals("Edit Component") && !actionName.equals("Send to Left") &&
+                        !actionName.equals("Send to Right") && !actionName.equals("Edit Group") &&
+                        !actionName.equals("Module Directory") && !actionName.equals("Grid") &&
+                        !actionName.equals("Delete") && !actionName.equals("Browse") && !actionName.equals("Split Line") &&
+                        !actionName.equals("Show Source") && !actionName.equals("Refresh") && !actionName.equals("Edit Maximized") &&
+                        !actionName.equals("Reload Page") && !actionName.equals("Edit") && !actionName.equals("Generate SQL") &&
+                        !actionName.equals("Set DEFAULT") && !actionName.equals("Level up") && !actionName.equals("Tapestry Class") &&
+                        !actionName.equals("Actual Size") && !actionName.equals("Apply Current Layout") &&
+                        !actionName.equals("Next Parameter") && !actionName.equals("Tag to Documentation Navigation") &&
+                        !actionName.equals("Commit Message History") && !actionName.equals("Exclude") &&
+                        !actionName.equals("Show Diff Settings Popup") && !actionName.equals("Desktop Directory") &&
+                        !actionName.equals("Next Template Variable or Finish In-Place Refactoring") && !actionName.equals("Indent Selection") &&
+                        !macShortcutFormatted.equals("Tag to Documentation Navigation") &&
+                        !winShortcutFormatted.isEmpty() && !macShortcutFormatted.isEmpty() &&
                         action.getTemplatePresentation().getText().length() > 1) {
                     if (started) {
                         stringBuilder.append(",\n");
                     }
-                    stringBuilder.append("\t\t\t\"").append(action.getTemplatePresentation().getText()).append("\": [").append(winShortcutFormatted).append(", ").append(macShortcutFormatted).append("]");
+                    stringBuilder.append("\t\t\t\"").append(actionName).append("\": [").append(winShortcutFormatted).append(", ").append(macShortcutFormatted).append("]");
                     started = true;
                 }
             }
@@ -77,6 +129,7 @@ public class ShortcutExporterAction extends AnAction {
                 replaceAll("ctrl alt", "ctrl + alt").
                 replaceAll("meta alt", "meta + alt").
                 replaceAll("shift meta", "shift + meta").
+                replaceAll("ctrl meta", "ctrl + meta").
                 replaceAll("shift meta alt", "shift + meta + alt").
                 replaceAll("shift", "Shift").
                 replaceAll("ctrl", "Ctrl").
